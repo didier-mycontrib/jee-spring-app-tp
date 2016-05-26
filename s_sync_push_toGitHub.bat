b7cbf493c29a0a874657c395c8824fc482e7ae5c
formation <formation@ici_ou_la.fr>
2016-05-26 21:02:53 +0200

    with good github url

--- s_sync_push_toGitHub.bat
+++ s_sync_push_toGitHub.bat
@@ -1,4 +1,4 @@
-git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/jjee-spring-app-tp.git
+git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/jee-spring-app-tp.git
 REM didier-mycontrib / gh14.....sm..x / didier@d-defrance.fr
 git push -u GitHubMyContribOrigin master
 pause
